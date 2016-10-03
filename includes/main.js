775e31d3b1de798b37441a132324d274b2cec426
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-03 17:41:57 +0200

    added a map! Of london, unfortunately

--- includes/main.js
+++ includes/main.js
@@ -11,3 +11,12 @@ $("#portf1").click(function(){
 
 $("#imgSelma").fadeOut( 5000 )
 $("#imgSelma").fadeIn( 5000 )
+
+// Contact: map
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
