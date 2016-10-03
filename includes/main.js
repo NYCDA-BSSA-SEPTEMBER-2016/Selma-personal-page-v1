5828a5375f580ddf219c364449c3ac3392ebed72
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-03 20:58:26 +0200

    fixed my map with a marker! Sweeeet!

--- includes/main.js
+++ includes/main.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 $("div #portf1tekst").hide();
 
 $("#portf1").click(function(){
@@ -13,10 +17,28 @@ $("#imgSelma").fadeOut( 5000 )
 $("#imgSelma").fadeIn( 5000 )
 
 // Contact: map
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(52.341093, 4.82315496), zoom: 15
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+ function initMap() {
+        var bssa = {lat: 52.341093, lng: 4.82315496};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: bssa
+        });
+        var marker = new google.maps.Marker({
+          position: bssa,
+          map: map
+        });
+      }
+
+
+
+// function myMap() {
+//   var mapCanvas = document.getElementById("map");
+//   var mapOptions = {
+//     center: new google.maps.LatLng(52.341093, 4.82315496), zoom: 15
+//   };
+//   var map = new google.maps.Map(mapCanvas, mapOptions);
+//           var marker = new google.maps.Marker({
+//           position: uluru,
+//           map: map
+//         });
+// }
