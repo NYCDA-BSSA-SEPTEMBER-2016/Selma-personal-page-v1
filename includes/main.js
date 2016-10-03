f7a4c098d40704070fb445ad3df3ef0b8677caa0
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-03 17:31:38 +0200

    changed a lot, got not much further unfortunately

---
+++ includes/main.js
@@ -0,0 +1,13 @@
+
+$("div #portf1tekst").hide();
+
+$("#portf1").click(function(){
+	$("#portf1tekst").fadeIn(1000);
+});
+$("#portf1").click(function(){
+	$("#portf1tekst").fadeOut(1000);
+});
+
+
+$("#imgSelma").fadeOut( 5000 )
+$("#imgSelma").fadeIn( 5000 )
